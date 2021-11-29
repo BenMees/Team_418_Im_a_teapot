12fab97c678daa5e13892cbbd7b5ff8e5ceeec2c
RevenkoAlexei <work.alexei.revenko@gmail.com>
2021-11-29 11:27:01 +0100

    (AR)(Logger Setup)(adding logger in bookController - establishing log folder for log outputs - gitignore for log files)

--- src/main/java/com/team418/api/BookController.java
+++ src/main/java/com/team418/api/BookController.java
@@ -1,8 +1,15 @@
 package com.team418.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
+    private final static Logger TEST_LOGGER = LoggerFactory.getLogger(BookController.class);
+
+    public BookController() {
+        TEST_LOGGER.info("BookController Creation");
+    }
 
 }
