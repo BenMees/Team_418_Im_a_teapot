b5db2239a49df78824a0b9ca5ba38b0111b1f741
enoirhom <emil.noirhomme@student.unamur.be>
2021-11-30 14:29:09 +0100

    (EN, JM) (Story 9: Register Librarian) -add tests in SecurityServiceTest -finish the story

--- src/test/java/com/team418/services/security/SecurityServiceTest.java
+++ src/test/java/com/team418/services/security/SecurityServiceTest.java
@@ -1,11 +1,15 @@
 package com.team418.services.security;
 
 import com.team418.domain.Feature;
-import com.team418.domain.user.Librarian;
+import com.team418.exception.UnauthorizedException;
+import com.team418.exception.UnknownUserException;
 import com.team418.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 
 class SecurityServiceTest {
@@ -25,7 +29,6 @@ class SecurityServiceTest {
 
         // WHEN
         Assertions.assertThatNoException().isThrownBy(() -> securityService.validate(authorization, Feature.CREATE_LIBRARIAN));
-
     }
 
     @Test
@@ -33,10 +36,17 @@ class SecurityServiceTest {
         // GIVEN
         String authorization = "Basic ZGVmYXVsdEBzd2l0Y2hmdWxseS5jb206Ym9ucXNkZg==";
 
-        // WHEN
+        // THEN
+        Assertions.assertThatExceptionOfType(UnauthorizedException.class).isThrownBy(() -> securityService.validate(authorization, Feature.REGISTER_NEW_BOOK));
+    }
+
+    @Test
+    void validate_givenAuthorizationThatIsInvalidAndCorrectAccess_thenNoExceptionIsThrown() {
+        // GIVEN
+        String authorization = "Basic " + Base64.getEncoder().encodeToString("jon@snow.com:password".getBytes(StandardCharsets.UTF_8));
 
         // THEN
-        securityService.validate(authorization, Feature.CREATE_LIBRARIAN);
+        Assertions.assertThatExceptionOfType(UnknownUserException.class).isThrownBy(() -> securityService.validate(authorization, Feature.REGISTER_NEW_BOOK));
     }
 
 }
