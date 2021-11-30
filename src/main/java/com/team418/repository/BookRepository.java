b5d659396fea33994cb8f6bd4b95f8e5d5f55f52
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-11-30 10:22:46 +0100

    (TD, TS) (Story 10A) (Fixed returning Book on save & tested all current features using Postman)

--- src/main/java/com/team418/repository/BookRepository.java
+++ src/main/java/com/team418/repository/BookRepository.java
@@ -1,4 +1,6 @@
 package com.team418.repository;
+
+import com.team418.domain.Author;
 import com.team418.domain.Book;
 import org.springframework.stereotype.Repository;
 
@@ -12,11 +14,11 @@ public class BookRepository {
 
     public BookRepository() {
         books = new ConcurrentHashMap<>();
-//        Book book1 = new Book("1","lskdj",new Author("sdf","sdf"),"df");
-//        Book book2 = new Book("2","lskdj",new Author("sdf","sdf"),"df");
-//
-//        this.saveBook(book1);
-//        this.saveBook(book2);
+        Book book1 = new Book("1", "lskdj", new Author("sdf", "sdf"), "df");
+        Book book2 = new Book("2", "lskdj", new Author("sdf", "sdf"), "df");
+
+        this.saveBook(book1);
+        this.saveBook(book2);
     }
 
     public Map<String, Book> getBooks() {
@@ -24,7 +26,8 @@ public class BookRepository {
     }
 
     public Book saveBook(Book book) {
-        return books.put(book.getUniqueId(), book);
+        books.put(book.getUniqueId(), book);
+        return book;
     }
 
     public Book getBook(String uniqueId) {
