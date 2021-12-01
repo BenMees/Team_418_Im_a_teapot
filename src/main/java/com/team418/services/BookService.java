de7c5b5a97a32f36df8c7d6524c35a1907641701
jmunoa <munoz.joachim.a@gmail.com>
2021-12-01 10:59:30 +0100

    (MJ,EN)(Story 3: Search a book by ISBN)(getBookByIsbn in BookService)

--- src/main/java/com/team418/services/BookService.java
+++ src/main/java/com/team418/services/BookService.java
@@ -3,7 +3,11 @@ package com.team418.services;
 import com.team418.domain.Book;
 import com.team418.repository.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.*;
 
 @Service
 public class BookService {
