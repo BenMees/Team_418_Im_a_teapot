97a3332262199dd8b3d7e2956f905a56d67d5f6e
jmunoa <munoz.joachim.a@gmail.com>
2021-12-01 11:02:20 +0100

    Changes by emilnoirhomme

--- src/main/java/com/team418/services/BookService.java
+++ src/main/java/com/team418/services/BookService.java
@@ -28,4 +28,29 @@ public class BookService {
     public Map<String, Book> getBooks() {
         return bookRepository.getBooks();
     }
+
+    public List<Book> getBooksByIsbn(String regex) {
+        List<Book> books = new ArrayList<>();
+        Pattern pattern = Pattern.compile(regex);
+
+        for (Book book : bookRepository.getBooks().values()) {
+            Matcher matcher = pattern.matcher(book.getIsbn());
+            if (matcher.matches()) {
+                books.add(book);
+            }
+        }
+
+        return books;
+    }
+//    public List<Book> getBooksByIsbn(String regex) {
+//        List<Book> books = new ArrayList<>();
+//
+//        for (Book book : bookRepository.getBooks().values()) {
+//            if (book.getIsbn().contains(regex)) {
+//                books.add(book);
+//            }
+//        }
+//
+//        return books;
+//    }
 }
