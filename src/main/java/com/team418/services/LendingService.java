378feb39b5a5fcc1b0031c327aa5a729f9767df1
RevenkoAlexei <work.alexei.revenko@gmail.com>
2021-12-02 09:41:41 +0100

    (AR + BM)(Story 11 : Lending a Book) (  - adding the check for two books having the same isbn  - checking that a deleted book should not be lendable  - adding two new exceptions for these two situations
     )

--- src/main/java/com/team418/services/LendingService.java
+++ src/main/java/com/team418/services/LendingService.java
@@ -2,7 +2,9 @@ package com.team418.services;
 
 import com.team418.domain.lending.Lending;
 import com.team418.repository.LendingRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LendingService {
     private final LendingRepository lendingRepository;
 
