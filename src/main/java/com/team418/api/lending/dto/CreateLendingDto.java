fc4ef04e8dd315b9bbf6149b89f773fd652f1d79
RevenkoAlexei <work.alexei.revenko@gmail.com>
2021-12-02 14:50:45 +0100

    (TEAM)(Story 11 : Lending a Book) (   - test finished for LendingController  )

--- src/main/java/com/team418/api/lending/dto/CreateLendingDto.java
+++ src/main/java/com/team418/api/lending/dto/CreateLendingDto.java
@@ -2,34 +2,5 @@ package com.team418.api.lending.dto;
 
 import java.util.Objects;
 
-public final class CreateLendingDto {
-    private final String isbn;
-
-    public CreateLendingDto(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String isbn() {
-        return isbn;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (CreateLendingDto) obj;
-        return Objects.equals(this.isbn, that.isbn);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(isbn);
-    }
-
-    @Override
-    public String toString() {
-        return "CreateLendingDto[" +
-                "isbn=" + isbn + ']';
-    }
-
+public record CreateLendingDto(String isbn) {
 }
